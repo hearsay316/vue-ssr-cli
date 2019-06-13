c35330a5eb9e1f71e1c14e1069ee8b40434223dd
hearsay316 <908388349@qq.com>
2019-06-13 23:26:32 +0800

    1 后端路由测试

--- src/components/Foo.vue
+++ src/components/Foo.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         foo测试
+        <ul>
+            <li v-for="i of item">{{i}}</li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Foo"
+        name: "Foo",
+        data(){
+            return{
+                item:[1,2,3,5]
+            }
+        }
     }
 </script>
 
